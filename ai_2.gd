878e3360081381d099ce17d46e0b54d4eea540f8
mileswangyc <mileswangyc@gmail.com>
2025-09-27 19:09:49 -0400

    AI Fix

--- ai_2.gd
+++ ai_2.gd
@@ -1,12 +1,24 @@
 extends CharacterBody2D
 
-@export var speed: float = 400.0  # pixels per second
+@export var speed: float = 100.0
+@export var move_time: float = 1.0
 
-func _physics_process(delta: float) -> void:
+var direction_angle
+var timer: float = 0.0
+
+func _ready():
+	randomize()
+	pick_new_direction()
+
+func _physics_process(delta):
+	timer -= delta
+	if timer <= 0:
+		pick_new_direction()
+
+	velocity = direction_angle * speed
 	
-	var direction = Vector2(randf_range(-1,1), randf_range(-1,1)).normalized()
-	
-
-	velocity = direction * speed
-
 	move_and_slide()
+
+func pick_new_direction():
+	direction_angle = Vector2(1,randf_range(-1,1))
+	timer = move_time
