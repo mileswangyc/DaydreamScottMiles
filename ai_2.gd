f62ee8351bc58073af9b7d5e9df6a8c364bc6fc3
mileswangyc <mileswangyc@gmail.com>
2025-09-27 20:35:56 -0400

    Changed Camera Zoom & NPC dir

--- ai_2.gd
+++ ai_2.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 @export var speed: float = 100.0
 @export var move_time: float = 1.0
 
-var direction_angle
+var direction_V2
 var timer: float = 0.0
 
 func _ready():
@@ -15,10 +15,12 @@ func _physics_process(delta):
 	if timer <= 0:
 		pick_new_direction()
 
-	velocity = direction_angle * speed
+	velocity = direction_V2 * speed
+	
+	rotation = direction_V2.angle() +PI
 	
 	move_and_slide()
 
 func pick_new_direction():
-	direction_angle = Vector2(1,randf_range(-1,1))
+	direction_V2 = Vector2(1,randf_range(-1,1))
 	timer = move_time
