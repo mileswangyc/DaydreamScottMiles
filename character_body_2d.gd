70904052535ad76cbf71dddb6d0e6b9de8e06476
mileswangyc <mileswangyc@gmail.com>
2025-10-12 22:30:31 -0400

    e

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -148,9 +148,8 @@ func _on_car_pickup_body_entered(body: Node2D) -> void:
 
 func _on_tv_pickup_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		if $"TV's Left".text != "0":
-			$"../CanvasLayer/Win".visible = true
-			$Win.play()
+		$"../CanvasLayer/Win".visible = true
+		$Win.play()
 		
 
 
